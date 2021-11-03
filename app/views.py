f7c8a7ee17a03fea799f60beeaef1bfdb9097cf2
qsummerford <qsummerford22@basecampcodingacademy.org>
2021-11-03 16:03:06 -0500

    I fixed the registration and login

--- app/views.py
+++ app/views.py
@@ -7,31 +7,40 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from .models import *
 from .forms import CreateUserForm
-from .decorators import authenticated_user, allowed_users, admin_only
+from .decorators import unauthenticated_user, allowed_users
 # Create your views here.
+
 def register(request):
-    form = UserCreationForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-    context = {'form': form}
-    return render (request, 'register.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = UserCreationForm()
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect ('login')
+        context = {'form': form}
+        return render (request, 'register.html', context)
+
 
 def login_user(request):
-    if request.method == 'POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username or Password is incorrect')
-    
-    context = {}
-    return render(request, 'login.html', context)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+        
+        context = {}
+        return render(request, 'login.html', context)
 
 def logoutUser(request):
     logout(request)
